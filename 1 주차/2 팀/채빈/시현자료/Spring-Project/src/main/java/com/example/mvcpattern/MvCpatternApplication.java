b634c727ba4f5b7bc847b3b7e7a2caa31bfbfa8e
chaeb03 <20231882@daejin.ac.kr>
2025-05-29 19:38:11 +0900

    Add files via upload

---
+++ 1 주차/2 팀/채빈/시현자료/Spring-Project/src/main/java/com/example/mvcpattern/MvCpatternApplication.java
@@ -0,0 +1,13 @@
+package com.example.mvcpattern;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MvCpatternApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MvCpatternApplication.class, args);
+    }
+
+}

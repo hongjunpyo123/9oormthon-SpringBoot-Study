eb884675e02a91ece358d73615a59f69210c9d11
june <0721hjp@naver.com>
2025-06-05 12:20:18 +0900

    init

--- 2주차/1 팀/준표/시현자료/Spring-Project/crud/src/main/java/com/study/crud/repository/Repository.java
+++
@@ -1,8 +0,0 @@
-package com.study.crud.repository;
-
-import com.study.crud.entity.UserEntity;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface Repository extends JpaRepository<UserEntity, String> {
-    boolean existsById(String id);
-}
